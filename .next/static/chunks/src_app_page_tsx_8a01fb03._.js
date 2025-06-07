20f63bf1c90c5070aa06912b47e4e489902b0962
Fernando Teguh <mobile.developer2@loanmarket.co.id>
2025-06-07 13:06:33 +0700

    branstalk

--- .next/static/chunks/src_app_page_tsx_8a01fb03._.js
+++ .next/static/chunks/src_app_page_tsx_8a01fb03._.js
@@ -6,7 +6,7 @@
   "path": "static/chunks/src_app_page_tsx_8a01fb03._.js",
   "chunks": [
     "static/chunks/src_bdaf10d1._.js",
-    "static/chunks/node_modules_37798fd1._.js",
+    "static/chunks/node_modules_d0635e30._.js",
     "static/chunks/node_modules_slick-carousel_slick_eab2732a._.css"
   ],
   "source": "dynamic"
